60f839871d9b4ad0fe384363b9fd08141da72ec2
ysa01 <74618676+ysa01@users.noreply.github.com>
2021-05-03 15:10:04 +0300

    ilk

---
+++ intro/src/intro/Main.java
@@ -0,0 +1,24 @@
+package intro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     //System.out.println("hello word");
+		String [] diziler= {
+				"anne",
+				"baba",
+				"aunt",
+				"review"
+		};
+		for(String dizi:diziler)
+		{
+			System.out.println(dizi);
+		}
+		for(int i=0; 0<diziler.length; i++)
+		{
+			System.out.println();
+		}
+	}
+
+}
